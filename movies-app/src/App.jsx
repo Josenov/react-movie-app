e96fa2f59b32dd2ab8f3e793797f4c67fa4d6a30
Jose Maria Novillo <josenovillo88@gmail.com>
2023-10-08 00:21:42 -0300

    project dependencies installed

---
+++ movies-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-3xl flex justify-center items-center text-white'>
+        <h1 >MovieMania</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
